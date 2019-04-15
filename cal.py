21934e858ecc14ddeeb87e00377965a36614e4b4
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-04-15 16:25:45 +0430

    Adding nCR and the probability of rank I

--- cal.py
+++ cal.py
@@ -3,12 +3,15 @@
 # coded by Hosein Kangavar Nazari
 import numpy as np
 import math
+import operator as op
+from functools import reduce
 
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, range(n, n-r, -1), 1)
+    denom = reduce(op.mul, range(1, r+1), 1)
+    return numer / denom
 
-def combination(N, x):
-    numerator = math.factorial(N)
-    denominator = math.factorial(N-x) * math.factorial(x)
-    return numerator/denominator
 
 
 # q denotes Finite field size , m received packet in decoding matrix, K is number of symbols to decode
@@ -16,13 +19,22 @@ def full_rank_probability(q, m, k):
     if m < k:
         return 0
     answer = 1
-    for i in range(k-1):
+    for i in range(k):
         answer *= 1 - pow(q, i-m)
     return answer
 
-def i_rank_probability
+
+def i_rank_probability(q, mu, k, i):
+    preEquation = 1 / pow(q, (mu - i)*(k-i))
+    answer = preEquation
+    for l in range(i):
+        numeratorLeft = 1 - pow(q, l - mu)
+        numeratorRight = 1 - pow(q, l - k)
+        denominator = 1 - pow(q, l-i)
+        answer *= (numeratorLeft*numeratorRight)/denominator
+    return answer
 
 
 if __name__ == "__main__":
     test = full_rank_probability(2, 15, 10)
-    print(test)
+    print(ncr(5,3))
