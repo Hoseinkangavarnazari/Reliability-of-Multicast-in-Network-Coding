74f5ac1d29a02458dd8754a15cab8ed9e31f5f93
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-05-26 23:31:09 +0430

    sync

--- generateReducedMethod.py
+++ generateReducedMethod.py
@@ -1,23 +1,32 @@
 def reducedStateGenerator(List, TOTAL_NUMBER):
     '''Generate all possible Share (not final set or final state for PLE)'''
 
+    ''' if multiprocessing is needed must be start at here, each round must be done with one processor'''
+    counter = 0 
+    if(TOTAL_NUMBER == 12):
+        counter +=1
+        print("Number {}".format(counter))
+    
     answerSets = []
     if(len(List) == 1):
-        # make 10 array and each array fill with 0 to total transmissions
-        # [[0], [1], [2], [3]]
+        answerSets.append([TOTAL_NUMBER])
+    else:
         for i in range(TOTAL_NUMBER+1):
-            answerSets.append([i])
+            CurrentState = [i]
+            allPossibleSet = reducedStateGenerator(
+                List[0:len(List)-1], TOTAL_NUMBER - i)
 
-    for i in range(TOTAL_NUMBER+1):
-        CurrentState = [i]
-        allPossibleSet = reducedStateGenerator(
-            List[0:len(List)-1], TOTAL_NUMBER - i)
-        
-        for tempSet in allPossibleSet:
-            answerSets.append(tempSet + CurrentState)
-            
-    return [[],[]]
+            for tempSet in allPossibleSet:
+                answerSets.append(tempSet + CurrentState)
+
+    return answerSets
 
 
 if __name__ == "__main__":
-    pass
+    NUMBER_OF_SYMBOLS = 5
+    NUMBER_OF_TOTAL_TRANSMISSION = 19
+
+    NUMBER_OF_RECEIVER = 13
+    test = reducedStateGenerator([0 for i in range(
+        NUMBER_OF_SYMBOLS, NUMBER_OF_TOTAL_TRANSMISSION + 1)], NUMBER_OF_RECEIVER)
+    print("here")
